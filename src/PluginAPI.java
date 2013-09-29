688a275283f4f280fd47aecaa74aeb185f9330bd
Robbie Cooper <cooperra@rose-hulman.edu>
2013-09-29 14:36:39 -0400

    Add some PluginAPI stubs. (Need UI)

--- src/PluginAPI.java
+++ src/PluginAPI.java
@@ -1,16 +1,32 @@
+import java.awt.Canvas;
 import java.awt.event.ActionListener;
 
 
 public class PluginAPI {
 	private IPlugin plugin;
 	private EventManager em;
+	private /*TODO*/Object ui;
 	
-	public PluginAPI(IPlugin plugin, EventManager em) {
+	public PluginAPI(IPlugin plugin, EventManager em, /*TODO*/Object ui) {
 		this.plugin = plugin;
 		this.em = em;
+		this.ui = ui;
 	}
 	
 	public boolean registerEvent(String eventName, ActionListener al) {
 		return em.registerEvent(eventName, plugin.getId(), al);
 	}
+	
+	public boolean unRegisterEvent(String eventName) {
+		return em.unRegisterEvent(eventName, plugin.getId());
+	}
+	
+	public Canvas getCanvas() {
+		return null; //TODO return ui.canvas;
+	}
+	
+	public void displayStatus(String status) {
+		//TODO ui.statusbar.setText(status);
+	}
+	
 }
